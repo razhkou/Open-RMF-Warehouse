cc36bdc873f9bb9875b93b8013cc09beb0ac6634
razhkou <co2copydelete@gmail.com>
2025-04-01 19:33:54 +0300

    Add files via upload

---
+++ rms_ws_wh/build/rmf_demos/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniele/rms_ws_wh/src/rmf_demos_warehouse/rmf_demos
+# Build directory: /home/daniele/rms_ws_wh/build/rmf_demos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
