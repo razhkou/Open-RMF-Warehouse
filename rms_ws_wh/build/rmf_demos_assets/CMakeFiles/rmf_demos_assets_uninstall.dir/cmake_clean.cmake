07ae655b2fa76d86887a73cc1dbc84133f80bcb5
razhkou <co2copydelete@gmail.com>
2025-04-01 19:35:09 +0300

    Add files via upload

---
+++ rms_ws_wh/build/rmf_demos_assets/CMakeFiles/rmf_demos_assets_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rmf_demos_assets_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rmf_demos_assets_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rms_ws_wh/build/rmf_demos_assets/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniele/rms_ws_wh/src/rmf_demos_warehouse/rmf_demos_assets
+# Build directory: /home/daniele/rms_ws_wh/build/rmf_demos_assets
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
