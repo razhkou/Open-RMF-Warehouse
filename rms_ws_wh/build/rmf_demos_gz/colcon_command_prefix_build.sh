7cd8c0ea3374a7ec874f1214d161878acaa1ce49
razhkou <co2copydelete@gmail.com>
2025-04-01 19:38:00 +0300

    Add files via upload

---
+++ rms_ws_wh/build/rmf_demos_gz/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/daniele/rms_ws_wh/install/rmf_demos_assets/share/rmf_demos_assets/package.sh"
+. "/home/daniele/rms_ws_wh/install/rmf_demos_fleet_adapter/share/rmf_demos_fleet_adapter/package.sh"
+. "/home/daniele/rms_ws_wh/install/rmf_demos_maps/share/rmf_demos_maps/package.sh"
+. "/home/daniele/rms_ws_wh/install/rmf_demos_tasks/share/rmf_demos_tasks/package.sh"
+. "/home/daniele/rms_ws_wh/install/rmf_demos/share/rmf_demos/package.sh"
