a9312d41d5a78858e3dfa121dc1a175e5b1aa13d
razhkou <co2copydelete@gmail.com>
2025-04-01 20:01:49 +0300

    Add files via upload

---
+++ rms_ws_wh/build/rmf_demos_maps/CMakeFiles/generate_warehouse_nav_graphs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_warehouse_nav_graphs"
+  "maps/warehouse/nav_graphs/phony"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/generate_warehouse_nav_graphs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rms_ws_wh/build/rmf_demos_maps/CMakeFiles/rmf_demos_maps_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rmf_demos_maps_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rmf_demos_maps_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
